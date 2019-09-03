f4bc92f487ef70ef5ec897ab2c0e90655f0e1ef7
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-09-03 19:25:22 +0200

    Add gitignore file, main class, class with method and testclass.

---
+++ src/main/java/pl/javastart/mavendemo/Main.java
@@ -0,0 +1,11 @@
+package pl.javastart.mavendemo;
+
+import pl.javastart.mavendemo.pl.javastart.mavendemo.Euler6Service;
+
+public class Main {
+    public static void main(String[] args) {
+        Euler6Service euler6Service = new Euler6Service();
+        System.out.println(euler6Service.subtractionOfSquares(100));
+    }
+
+}
---
+++ src/main/java/pl/javastart/mavendemo/pl/javastart/mavendemo/Euler6Service.java
@@ -0,0 +1,20 @@
+package pl.javastart.mavendemo.pl.javastart.mavendemo;
+
+public class Euler6Service {
+
+    public int subtractionOfSquares(int a){
+        int sumOfSquares = 0;
+        int sum = 0;
+        for (int i = 0; i <= a ; i++) {
+            int result = i*i;
+            sumOfSquares+= result;
+        }
+        for (int i = 0; i <= a ; i++) {
+            sum+=i;
+        }
+        int squareOfSums = sum*sum;
+        int subtraction = squareOfSums - sumOfSquares;
+        return subtraction;
+    }
+
+}
