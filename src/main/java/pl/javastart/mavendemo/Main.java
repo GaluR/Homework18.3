65d7e9c36721a78b85db62cfc445eb78eee0c06b
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-09-04 11:10:31 +0200

    Correct pom file, name of service class.

--- src/main/java/pl/javastart/mavendemo/Main.java
+++ src/main/java/pl/javastart/mavendemo/Main.java
@@ -1,10 +1,8 @@
 package pl.javastart.mavendemo;
 
-import pl.javastart.mavendemo.pl.javastart.mavendemo.Euler6Service;
-
 public class Main {
     public static void main(String[] args) {
-        Euler6Service euler6Service = new Euler6Service();
+        EulerService euler6Service = new EulerService();
         System.out.println(euler6Service.subtractionOfSquares(100));
     }
 
